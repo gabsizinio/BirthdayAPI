2519b565b0e88cbb193279b05c4acac499eeb138
gabsizinio <gsizinio@gmail.com>
2025-02-16 11:11:03 -0300

    Adicionado o Scraping para conseguir os filmes mais populares

--- list_popular.py
+++ list_popular.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import re
 
 def list_popular_animes():
     #Fazer a requisição para obter o HTML da página
@@ -65,12 +66,51 @@ def list_popular_series():
     tags = soup.find_all("h3", class_="ipc-title__text")
 
     series = [tag.text.strip() for tag in tags if tag.text.strip()]
+
+    series = series[:-13]
     
+    series = [re.sub(r"^\d+\.\s*", "", serie) for serie in series]
+
     for serie in series:
         print(serie)
-    return series
 
+def list_popular_movies():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")  
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/110.0.0.0 Safari/537.36")
+
+
+    
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    # Abre a página do IMDB
+    url = "https://www.imdb.com/chart/top/?sort=num_votes%2Cdesc"
+    driver.get(url)
+
+    # Espera um tempo para a página carregar completamente (caso precise)
+    time.sleep(3)
+
+    # Pega o HTML da página depois do carregamento completo do JavaScript
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    #print(soup.prettify())
+    # Fecha o navegador
+    driver.quit()
+
+    # Encontra os títulos das séries
+    tags = soup.find_all("h3", class_="ipc-title__text")
+
+    movies = [tag.text.strip() for tag in tags if tag.text.strip()]
+
+    movies = movies[:-13]
+    
+    movies = [re.sub(r"^\d+\.\s*", "", movie) for movie in movies]
+
+    for movie in movies:
+        print(movie)
 
 
 #list_popular_animes()
-list_popular_series()
+#list_popular_series()
+#list_popular_movies()
